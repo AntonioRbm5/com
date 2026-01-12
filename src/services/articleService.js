92c368b0d098b27ebf8ccda984f01a6a30627011
AntonioRbm5 <antoniorbm5@gmail.com>
2026-01-12 16:42:43 +0300

    add contact

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -4,5 +4,6 @@ import api from "./api";
 export const getAllArticles = () => api.get("/article/all");
 export const getArticleById = (id) => api.get(`/article/${id}`);
 export const createArticle = (data) => api.post("/article/create", data);
+export const searchArticle = (name) => api.get(`/article/search?article_name=${name}`);
 export const updateArticle = (id, data) => api.put(`/article/update/${id}`, data);
 export const deleteArticle = (id) => api.delete(`/article/delete/${id}`);
