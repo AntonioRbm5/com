6faf7b571667876b206d50de2cef6c6aeb62c74e
AntonioRbm5 <antoniorbm5@gmail.com>
2026-01-12 17:09:07 +0300

    add stock mouvement service

--- src/services/stockMouvementService.js
+++ src/services/stockMouvementService.js
@@ -1,3 +1,8 @@
 import api from "./api";
 export const createStockMouvement = (data) => api.post(`/stock-mouvement/create`, data)
-// export const 
+export const getStockMouvementById = (id) => api.get(`/stock-mouvement/get/${id}`)
+export const getAllStockMouvement = () => api.get("/stock-mouvement/search")
+export const getStockMouvementByArticleId = (article_id) => api.get(`/stock-mouvement/search?article_id=${article_id}`) 
+export const getStockMouvementByReference = (mouvement_reference) => api.get(`/stock-mouvement/search?mouvement_reference=${mouvement_reference}`)
+export const updateStockMouvement = (data, id) => api.put(`/stock-mouvement/update/${id}`, data)
+export const deleteStockMouvement = (id) => api.delete(`/stock-mouvement/delete/${id}`)
