2439ab19ffc97174230b8f1931432353fc5b1d86
AntonioRbm5 <antoniorbm5@gmail.com>
2026-01-12 15:57:44 +0300

    add telecomm service

--- src/services/telecommunicationService.js
+++ src/services/telecommunicationService.js
@@ -2,5 +2,7 @@ import api from "./api";
 
 export const createTelecommunication = (data) => api.post("/telecommunication/create", data)
 export const getAllTelecommunication = () => api.get("/telecommunication/all")
-export const getTelecommunicationSearch = (keyword) => api.get(`/telecommunication/search?keyword=${keyword}`)
-// export const updateTelecommunication = (data, id_telecomm) => api.put()
+export const getTelecommunicationSearchByEmail = (telecom_info_email) => api.get(`/telecommunication/search?telecom_info_email=${telecom_info_email}`)
+export const getTelecommunicationSearchByPhone = (telecom_info_tel) => api.get(`/telecommunication/search?telecom_info_tel=${telecom_info_tel}`)
+export const deleteTelecommunication = (id_telecom) => api.delete(`/telecommunication/delete/${id_telecom}`)
+export const updateTelecommunication = (data, id_telecom) => api.put(`/telecommunication/update/${id_telecom}`, data)
