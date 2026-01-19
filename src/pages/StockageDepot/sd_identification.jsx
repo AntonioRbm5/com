5247bdcb3450d9e68c44058d89bec30a0b48db2b
AntonioRbm5 <antoniorbm5@gmail.com>
2026-01-19 21:48:33 +0300

    init tiers interface

--- src/pages/StockageDepot/sd_identification.jsx
+++ src/pages/StockageDepot/sd_identification.jsx
@@ -28,6 +28,72 @@ const Sdidentifications = () => {
     };
     return (
        <form action="">
+        {/* ================= Identification ================= */}
+         <div className="card mb-4 shadow-sm">
+            <div className="card-header bg-light fw-semibold">
+            1. Identification
+            </div>
+            <div className="card-body">
+                <h6 className="mb-3 mt-4">Identification</h6>
+                {/* Intitulé */}
+                <div className="row mb-3 align-items-center">
+                    <label className="col-md-3 col-form-label">Intitulé</label>
+                    <div className="col-md-9">
+                    <input
+                        type="text"
+                        name="adresse"
+                        value={formData.adresse}
+                        onChange={handleChange}
+                        className="form-control"
+                    />
+                    </div>
+                </div>
+
+                {/* Responsable */}
+                <div className="row mb-3 align-items-center">
+                    <label className="col-md-3 col-form-label">Responsable depôt</label>
+                    <div className="col-md-9">
+                    <select className="form-control">
+                        <option value="" selected>--User Default--</option>
+                    </select>
+                    </div>
+                </div>
+
+                {/* Code */}
+                <div className="row mb-3">
+                    <div className="col-md-6">
+                    <div className="row align-items-center">
+                        <label className="col-md-4 col-form-label">Code dépot</label>
+                        <div className="col-md-8">
+                        <input
+                            type="text"
+                            name="code_postal"
+                            value={formData.code_postal}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-md-6">
+                    <div className="row align-items-center">
+                        <label className="col-md-4 col-form-label">Catégorie comptable</label>
+                        <div className="col-md-8">
+                        <input
+                            type="text"
+                            name="ville"
+                            value={formData.ville}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         {/* ================= Coordonnées ================= */}
         <div className="card mb-4 shadow-sm">
             <div className="card-header bg-light fw-semibold">
